b45d23ae167926e681e8f919ae9ad1fca1ff977c
JacobRose <95928931+JacobRose43@users.noreply.github.com>
2022-03-08 20:18:56 +0100

    update

--- data/MongooseBasics/index.js
+++ data/MongooseBasics/index.js
@@ -2,16 +2,15 @@ const mongoose = require('mongoose');
 
 const { Schema } = mongoose;
 
-// async function main() {
-// 	try {
-// 		return await mongoose.connect('mongodb://localhost:27017/movieApp');
-// 	} catch (error) {
-// 		console.log('CAUGHT AN ERROR!');
-// 		console.log(`error is: ${error}`);
-// 	}
-// }
-
-mongoose.connect('mongodb://localhost:27017/movieApp');
+mongoose
+	.connect('mongodb://localhost:27017/movieApp')
+	.then(() => {
+		console.log('Connected to MongoDB');
+	})
+	.catch((error) => {
+		console.log('We have a problem!');
+		console.log(error);
+	});
 
 const movieSchema = new Schema({
 	title: String,
@@ -22,19 +21,56 @@ const movieSchema = new Schema({
 
 const Movie = mongoose.model('Movie', movieSchema);
 
-// const IronMan = new Movie({
-// 	title: 'Iron Man',
-// 	director: 'Jon Favreau',
-// 	category: ['Action', 'Adventure', 'Sci-Fi'],
-// 	releaseDate: new Date(2008, 04, 30)
-// });
+const IronMan = new Movie({
+	title: 'Iron Man',
+	director: 'Jon Favreau',
+	category: ['Action', 'Adventure', 'Sci-Fi'],
+	releaseDate: new Date(2008, 04, 30)
+});
 
-// const Avengers = new Movie({
-// 	title: 'The Avengers',
-// 	director: 'Joss Whedon',
-// 	category: ['Action', 'Adventure', 'Sci-Fi'],
-// 	releaseDate: new Date(2012, 05, 11)
-// });
+const Avengers = new Movie({
+	title: 'The Avengers',
+	director: 'Joss Whedon',
+	category: ['Action', 'Adventure', 'Sci-Fi'],
+	releaseDate: new Date(2012, 05, 11)
+});
+
+// another option of inserting data, but less used
+
+// Movie.insertMany([
+// 	{
+// 		title: 'Captain America: The First Avenger',
+// 		director: 'Joe Johnston',
+// 		category: ['Action', 'Adventure', 'Sci-Fi'],
+// 		releaseDate: new Date(2011, 08, 05)
+// 	},
+// 	{
+// 		title: 'Captain Marvel',
+// 		director: 'Anna Boden & Ryan Fleck',
+// 		category: ['Action', 'Adventure', 'Sci-Fi'],
+// 		releaseDate: new Date(2019, 03, 08)
+// 	},
+// 	{
+// 		title: 'Cruella',
+// 		director: 'Craig Gillespie',
+// 		category: ['Comedy', 'Adventure', 'Crime'],
+// 		releaseDate: new Date(2021, 05, 28)
+// 	},
+// 	{
+// 		title: 'Nobody',
+// 		director: 'Ilya Naishuller',
+// 		category: ['Action', 'Crime', 'Drama'],
+// 		releaseDate: new Date(2021, 03, 26)
+// 	}
+// ])
+// 	.then((data) => {
+// 		console.log('Accepted!');
+// 		console.log(data);
+// 	})
+// 	.catch((error) => {
+// 		console.log('Denied');
+// 		console.log(error);
+// 	});
 
 // Avengers.save()
 // 	.then(() => {
